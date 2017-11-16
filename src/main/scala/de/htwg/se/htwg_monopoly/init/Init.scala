612523a9fca7516375b97dcf4e3db0ba9514cb6a
tabuechn <tabuechn@htwg-konstanz.de>
2017-11-16 14:33:57 +0100

    added Tui again

--- src/main/scala/de/htwg/se/htwg_monopoly/init/Init.scala
+++ src/main/scala/de/htwg/se/htwg_monopoly/init/Init.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.htwg_monopoly.init
 
 import de.htwg.se.htwg_monopoly.controller.Controller
-import de.htwg.se.htwg_monopoly.model.util.Util.{createGameField, readHTWGMonopoly}
+import de.htwg.se.htwg_monopoly.model.util.Util.readHTWGMonopoly
 import de.htwg.se.htwg_monopoly.model.fields.{Field, FieldGroup, Prices}
 import de.htwg.se.htwg_monopoly.model.game.{Game, GameField, Player}
 import de.htwg.se.htwg_monopoly.view.tui.Tui
@@ -13,16 +13,14 @@ object Init {
     player1.addField(Field("Fieldname1","grp1",Prices(1,2,3,4,5,Array(1,2,3,4,5))))
     player1.addField(Field("Fieldname2","grp2",Prices(1,2,3,4,5,Array(1,2,3,4,5))))
     val players = Array(player1, player2)
-    val game = readHTWGMonopoly()
-    println(game)
+    val gameField = readHTWGMonopoly()
+    println(gameField)
     println("done")
-    /*
-    val gameField = createGameField()
     val game = Game(gameField, players)
     val controller = Controller(game)
 
 
 
-    val tui: Tui = Tui(controller)*/
+    val tui: Tui = Tui(controller)
   }
 }
